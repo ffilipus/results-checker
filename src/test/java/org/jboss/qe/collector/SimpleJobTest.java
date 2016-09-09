1d3d7324039c5925284f106cdbc2a11e51dbd214
fjerabek <fanajerabek@seznam.cz>
2016-09-09 11:19:22 +0200

    JUnit test is now using files from resources folder

--- src/test/java/org/jboss/qe/collector/SimpleJobTest.java
+++ src/test/java/org/jboss/qe/collector/SimpleJobTest.java
@@ -1,16 +1,12 @@
 package org.jboss.qe.collector;
 
+import org.apache.cxf.helpers.IOUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.*;
 
 /**
  * Created by fjerabek on 8.9.16.
@@ -48,19 +44,21 @@ public class SimpleJobTest {
       PrintStream ps = new PrintStream(baos);
       old = System.out;
       System.setOut(ps);
+
+      String[] fileNames = new String[]{"jobPage.json","lastBuild.json","testReport.json"};
+      String[] resources = getContentResourceFiles(fileNames);
+      String[] names = getCachedNames(fileNames);
+
+      for (int i = 0; i < names.length; i++) {
+         Cache cache = new Cache(names[i]);
+         cache.add(resources[i]);
+      }
    }
 
    @Test
    public void testBasicFunctionality() {
       String test = "eap-70x-maven-repository-check-valid-POM-and-Metadata-files";
 
-
-      Path cashPath = Paths.get(System.getProperty("java.io.tmpdir"),test);
-      try {
-         FileOutputStream os = new FileOutputStream(cashPath.toString());
-      } catch (FileNotFoundException e) {
-         e.printStackTrace();
-      }
 
       new Main().main(new String[]{test});
       String output = baos.toString();
@@ -78,12 +76,49 @@ public class SimpleJobTest {
       Assert.assertEquals("Bad print of test status", " - PASSED: 308, FAILED: 2, SKIPPED: 0", split[13]);
       Assert.assertEquals("Error was not shown", " - infinispan-directory-provider-8.1.4.Final-redhat-1#infinispan-directory-provider-8.1.4.Final-redhat-1", split[14]);
       Assert.assertEquals("Error was not shown", " - infinispan-parent-8.1.4.Final-redhat-1#infinispan-parent-8.1.4.Final-redhat-1", split[15]);
-
    }
 
    @After
    public void cleanup() {
       System.out.flush();
       System.setOut(old);
+   }
+
+   private String[] getContentResourceFiles(String[] fileNames) {
+      String[] resources = new String[fileNames.length];
+      for (int i = 0; i < fileNames.length; i++) {
+         try (FileInputStream is = new FileInputStream(getClass().getClassLoader().getResource("simpleJob/" + fileNames[i]).getFile())) {
+            resources[i] = IOUtils.toString(is);
+         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      }
+      return resources;
+   }
+
+   private String[] removeSuffix(String[] fileNames) {
+      String[] rawFilenames = new String[fileNames.length];
+      for (int i = 0; i < fileNames.length; i++) {
+         String[] name = fileNames[i].split("\\.");
+         rawFilenames[i] = name[0];
+      }
+      return rawFilenames;
+   }
+
+   private String[] getCachedNames(String[] fileNames) {
+      String[] rawFileNames = removeSuffix(fileNames);
+      String[] names = new String[rawFileNames.length];
+      for (int i = 0; i < rawFileNames.length; i++) {
+         if (rawFileNames[i].equals("testReport")) {
+            names[i] = "result_checker_temp_eap-70x-maven-repository-check-valid-POM-and-Metadata-files"
+                + ".lastBuild." + rawFileNames[i] + ".api.json.index.html";
+         } else {
+            names[i] = "result_checker_temp_eap-70x-maven-repository-check-valid-POM-and-Metadata-files"
+                + "." + rawFileNames[i] + ".api.json.index.html";
+         }
+      }
+      return names;
    }
 }
