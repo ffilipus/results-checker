c11b97f08c794da771158bd295319b606df6f9cb
fjerabek <fanajerabek@seznam.cz>
2016-09-13 13:44:03 +0200

    Added filter result

---
+++ src/main/java/org/jboss/qe/collector/FilterResult.java
@@ -0,0 +1,22 @@
+package org.jboss.qe.collector;
+
+/**
+ * Created by fjerabek on 13.9.16.
+ */
+public class FilterResult {
+   private boolean match;
+   private String jobError;
+
+   public FilterResult(boolean match, String jobError){
+      this.jobError = jobError;
+      this.match = match;
+   }
+
+   public String getJobError() {
+      return jobError;
+   }
+
+   public boolean isMatch() {
+      return match;
+   }
+}
