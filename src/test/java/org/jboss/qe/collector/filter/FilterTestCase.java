07ffed9ff824c80e45cfb41a8b9d7ea4703ff735
Jan <dobesjan97@gmail.com>
2016-09-15 12:57:09 +0200

    FilterTestCase updated

--- src/test/java/org/jboss/qe/collector/filter/FilterTestCase.java
+++ src/test/java/org/jboss/qe/collector/filter/FilterTestCase.java
@@ -6,7 +6,7 @@ import org.jboss.qe.collector.FilterResult;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.Assert;
-import org.junit.Ignore;
+//import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.LinkedList;
@@ -38,10 +38,10 @@ public class FilterTestCase {
 
    @Test
    // TODO
-   @Ignore ("work in progress")
+   //@Ignore ("work in progress")
    public void test2() throws JSONException {
-      SetFailedTests1();
-      Filter filter = new TestFilter1();
+      SetFailedTests2();
+      Filter filter = new TestFilter2();
 
       // this cases should not be matched by a filter so final color should be red
       for (FailedTest ft : failedTests) {
@@ -134,6 +134,53 @@ public class FilterTestCase {
       buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/jdk=java18_default,label_exp=RHEL6%20&&%20x86%20&&%20eap-sustaining/";
       filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
 
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.common.e.test" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/jdk=java18_default,label_exp=RHEL6%20&&%20x86%20&&%20eap-sustaining/";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.common.e.test2" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/jdk=java18_default,label_exp=RHEL6%20&&%20x86%20&&%20eap-sustaining/";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.test.a" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/a";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.test.b" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/b";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.test.c" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/c";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "adfadf";
+      errorStackTrace = "Stack aaa";
+      className = "org.jboss.qe.cli.embed.gf" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/h";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "Error";
+      errorStackTrace = "Stack aaa";
+      className = "org.jboss.qe.cli.embed.qf" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/h";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      /*errorDetails = "Er";
+      errorStackTrace = "Stack aaa";
+      className = "org.jboss.qe.cli.embed.qf" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/h";
+      filteredTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));*/
 
       failedTests = new LinkedList<>();
 
@@ -141,6 +188,48 @@ public class FilterTestCase {
       errorStackTrace = "nejaky blaf";
       className = "org.jboss.qe.cli.embed.common.ModularServerTestCase" + "#" + "differentTest";
       buildUrl = "http://url.of.test.com/hudson/job/strasneHroznyNazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.common.ModularServerTestCase" + "#" + "aaa";
+      buildUrl = "http://url.of.test.com/hudson/job/strasneHroznyNazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.common.ModularServerTestCase" + "#" + "www";
+      buildUrl = "http://url.of.test.com/hudson/job/strasneHroznyNazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.common.ModularServerTestCase" + "#" + "det";
+      buildUrl = "http://url.of.test.com/hudson/job/strasneHroznyNazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.e.*";
+      buildUrl = "http://url.of.test.com/hudson/job/strasneHazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.test.c" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/r";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "nejaky blaf";
+      errorStackTrace = "nejaky blaf";
+      className = "org.jboss.qe.cli.embed.qq.";
+      buildUrl = "http://url.of.test.com/hudson/job/strasneHazevTestu";
+      failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
+
+      errorDetails = "Er";
+      errorStackTrace = "Stack aaa";
+      className = "org.jboss.qe.cli.embed.qf" + "#" + "multipleReloadOfRunningModeTest";
+      buildUrl = "http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/h";
       failedTests.add(new FailedTest(className, "buildUrl: " + buildUrl, testInfoFactory(errorDetails, errorStackTrace)));
    }
 
@@ -151,6 +240,35 @@ public class FilterTestCase {
                new FilterItem(Colour.YELLOW)
                      .addTest("testFailedJob.*")
                      .setErrorText("Testing filter by name"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.common.ModularServerTestCase" + "#" + "multipleReloadOfRunningModeTest")
+                      //Problem with matching from url
+                     .addUrl(".*http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/jdk=java18_default,label_exp=RHEL6%20&&%20x86%20&&%20eap-sustaining/")
+                     .setErrorText("Error"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.common.e.*")
+                     .addUrl(".*http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/jdk=java18_default,label_exp=RHEL6%20&&%20x86%20&&%20eap-sustaining/")
+                     .setErrorText("Multi tests matching"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.common.ModularServerTestCase#*")
+                     .setCategory("TEST")
+                     .setErrorText("Multiple errors match"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.test.*")
+                     .addUrl(".*http://jenkins.mw.lab.eng.bos.redhat.com/hudson/job/eap-70x-acceptance-multinode-rhel/test/.*")
+                     .setCategory("TEST")
+                     .setErrorText("Multiple errors match"),
+               new FilterItem(Colour.BLACK)
+                     .addTestMatcher((JSONObject errorDetails) -> errorDetails.get("errorDetails").equals("adfadf"))
+                     .setErrorText("Error"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.qf")
+                     .addTestMatcher((JSONObject errorDetails) -> errorDetails.get("errorDetails").equals("Error"))
+                     .setErrorText("Error"),
+               new FilterItem(Colour.YELLOW)
+                     .addTest("org.jboss.qe.cli.embed.test.b")
+                     .addTestMatcher((JSONObject errorDetails) -> errorDetails.get("errorDetails").equals("nejaky blaf"))
+                     .addTestMatcher((JSONObject errorStackTrace) -> errorStackTrace.get("errorStackTrace").equals("nejaky blaf"))
          };
 
          return coreFilter(failedTest, items);
