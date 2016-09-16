a229bd5112d955d979faf85c1fb908a17284c53b
Jan <dobesjan97@gmail.com>
2016-09-16 10:44:17 +0200

    TaskManager - map of tasks completed

--- src/main/java/org/jboss/qe/collector/TaskManager.java
+++ src/main/java/org/jboss/qe/collector/TaskManager.java
@@ -97,5 +97,98 @@ public class TaskManager {
       tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG").add("eap-7x-as-testsuite-test-integ-windows");
 
       tasks.put("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN", new ArrayList<>());
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN").add("eap-7x-as-testsuite-test-integ-hpux-secman");
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN").add("eap-7x-as-testsuite-test-integ-rhel-secman");
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN").add("eap-7x-as-testsuite-test-integ-solaris-secman");
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN").add("eap-7x-as-testsuite-test-integ-windows-secman");
+
+      tasks.put("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6", new ArrayList<>());
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6").add("eap-7x-as-testsuite-test-integ-rhel-ipv6");
+      tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6").add("eap-7x-as-testsuite-test-integ-windows-ipv6");
+
+      tasks.put("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN", new ArrayList<>());
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-hpux");
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-rhel");
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-solaris");
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-windows");
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-rhel-ipv6");
+      tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN").add("eap-7x-as-testsuite-test-mixed_domain-windows-ipv6");
+
+      tasks.put("eap_7x_as_testsuite_misc", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_misc").addAll(tasks.get("EAP_7x_AS_TESTSUITE_MISC"));
+
+      tasks.put("eap_7x_as_testsuite_test_unit", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_unit").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_UNIT"));
+
+      tasks.put("eap_7x_as_testsuite_test_core", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_core").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_UNIT"));
+      tasks.get("eap_7x_as_testsuite_test_core").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE_IPv6"));
+
+      tasks.put("eap_7x_as_testsuite_test_core_secman", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_core_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE"));
+      tasks.get("eap_7x_as_testsuite_test_core_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE_IPv6"));
+      tasks.get("eap_7x_as_testsuite_test_core_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE_SECMAN"));
+
+      tasks.put("eap_7x_as_testsuite_test_integ", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_integ").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG"));
+      tasks.get("eap_7x_as_testsuite_test_integ").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6"));
+
+      tasks.put("eap_7x_as_testsuite_test_integ_secman", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_integ_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG"));
+      tasks.get("eap_7x_as_testsuite_test_integ_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6"));
+      tasks.get("eap_7x_as_testsuite_test_integ_secman").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN"));
+
+      tasks.put("eap_7x_as_testsuite_test_mixed_domain", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_mixed_domain").addAll(tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN"));
+
+      tasks.put("eap_7x_as_testsuite_test_all", new ArrayList<>());
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_MISC"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_UNIT"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE_IPv6"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_CORE_SECMAN"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_IPv6"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_TEST_INTEG_SECMAN"));
+      tasks.get("eap_7x_as_testsuite_test_all").addAll(tasks.get("EAP_7x_AS_TESTSUITE_MIXED_DOMAIN"));
+
+      tasks.put("eap7_functional_tests", new ArrayList<>());
+      tasks.get("eap7_functional_tests").add("eap7-artemis-qe-internal-ts-functional-tests");
+      tasks.get("eap7_functional_tests").add("eap7-artemis-qe-internal-ts-functional-tests-matrix");
+
+      tasks.put("resteasy", new ArrayList<>());
+      tasks.get("resteasy").add("eap-7.x-resteasy-ts-request-verifier");
+
+      tasks.put("artemis", new ArrayList<>());
+      tasks.get("artemis").add("artemis-project-testsuite-prepare");
+
+      tasks.put("artemis_eus", new ArrayList<>());
+      tasks.get("artemis_eus").add("artemis-project-testsuite-rhel");
+      tasks.get("artemis_eus").add("artemis-project-testsuite-hpux");
+      tasks.get("artemis_eus").add("artemis-project-testsuite-solaris");
+      tasks.get("artemis_eus").add("artemis-project-testsuite-win");
+
+      tasks.put("eap7_ha", new ArrayList<>());
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-bridges");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-cluster");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-cluster-replicated-journal");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-cluster-replicated-journal-win");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-cluster-win");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-replicated-journal-rhel-ipv6");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-mdb");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-mdb-replicated-journal");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-network-disconnection");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-nfs");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-power-off-nfs");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-power-off-replicated-journal-aio");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-power-off-replicated-journal-nio");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-power-off-san");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-replicated-journal");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-replicated-journal-win");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-dedicated-xa-clients");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-replicated-journal-network-failures");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-colocated-cluster-newconf");
+      tasks.get("eap7_ha").add("eap7-artemis-ha-failover-jms20-clients");
    }
 }
