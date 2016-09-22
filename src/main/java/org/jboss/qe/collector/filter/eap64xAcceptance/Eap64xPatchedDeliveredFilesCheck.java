d8b428ce1065f7c322242d904eb52710c0f06855
Jiri Bilek <jbilek@redhat.com>
2016-09-22 11:42:08 +0200

    update filter

--- src/main/java/org/jboss/qe/collector/filter/eap64xAcceptance/Eap64xPatchedDeliveredFilesCheck.java
+++ src/main/java/org/jboss/qe/collector/filter/eap64xAcceptance/Eap64xPatchedDeliveredFilesCheck.java
@@ -21,8 +21,10 @@ public class Eap64xPatchedDeliveredFilesCheck extends AbstractFilter {
                   //.addTest("org.jboss.manu.units.eap.naming.CheckDeliveredFilesEapCp.CheckDeliveredFiles.root_files_unexpectedfilestest.*")
                   .addTestMatcher((JSONObject errorDetails) -> errorDetails.get("errorDetails").toString().matches(" in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR..zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-maven-repository.zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-patched.zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-patched-maven-repository.zip \n"))
                   .setErrorText("Files added by jenkins jobs - not delivered "),
-
-
+            new FilterItem(Colour.YELLOW)
+                  //.addTest("org.jboss.manu.units.eap.naming.CheckDeliveredFilesEapCp.CheckDeliveredFiles.root_files_unexpectedfilestest.*")
+                  .addTestMatcher((JSONObject errorDetails) -> errorDetails.get("errorDetails").toString().matches(" in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR..zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-maven-repository.zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-patched.zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-patched-maven-repository.zip \n in file:/home/hudson/static_build_env/eap/6.4....CP.CR./, unexpected file found: jboss-eap-6.4....CP.CR.-patched-rpm.zip \n"))
+                  .setErrorText("Files added by jenkins jobs - not delivered "),
       };
 
       return coreFilter(failedTest, items);
